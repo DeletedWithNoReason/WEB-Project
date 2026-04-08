df5ba03095d0ef45917428e8911ca9653950b969
vkddq <vovanoha42@gmail.com>
2026-04-08 08:19:00 +0300

    qwerty

--- config/users/admin.py
+++ config/users/admin.py
@@ -1,11 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
 
-from users.models import CustomUser
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'phone', 'role')
 
-# Register your models here.
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'phone', 'role')
+
+class CustomUserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
     list_display = ('email', 'phone', 'role', 'is_staff', 'is_active')
     list_filter = ('role', 'is_staff', 'is_active')
+
+    # This controls what you see when EDITING a user
+    fieldsets = (
+        (None, {'fields': ('email', 'phone', 'password')}),
+        ('User Info', {'fields': ('role',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active')}),
+    )
+
+    # This controls what you see when CREATING a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            # THE FIX: Explicitly list password1 and password2 here
+            'fields': ('email', 'phone', 'role', 'password1', 'password2'),
+        }),
+    )
+
     search_fields = ('email', 'phone')
     ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
